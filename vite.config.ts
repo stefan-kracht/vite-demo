eb08a540815275a533f3af99c56bfef5a62a7293
Stefan Kracht <stefan.kracht@direktiv.io>
2023-07-26 09:02:27 +0200

    add base option
    Signed-off-by: Stefan Kracht <stefan.kracht@direktiv.io>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  if (env.VITE_BASE) {
+    console.log("👌 found a base config:", env.VITE_BASE);
+  }
+
+  const baseconfig = env.VITE_BASE ? { base: env.VITE_BASE } : {};
+
+  return defineConfig({
+    plugins: [react()],
+    ...baseconfig,
+  });
+};
